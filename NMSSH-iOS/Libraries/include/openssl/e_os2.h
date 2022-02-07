5e77619880e70d0ea12aa1f34e2a544497231d9b
Ettore Gallina <egalnet@gallinaettore.com>
2022-02-07 13:04:35 +0100

    Removed unnecessary files

--- NMSSH-iOS/Libraries/include/openssl/e_os2.h
+++ NMSSH-iOS/Libraries/include/openssl/e_os2.h
@@ -237,10 +237,6 @@ typedef INT32 int32_t;
 typedef UINT32 uint32_t;
 typedef INT64 int64_t;
 typedef UINT64 uint64_t;
-# elif (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || \
-     defined(__osf__) || defined(__sgi) || defined(__hpux) || \
-     defined(OPENSSL_SYS_VMS) || defined (__OpenBSD__)
-#  include <inttypes.h>
 # elif defined(_MSC_VER) && _MSC_VER<1600
 /*
  * minimally required typdefs for systems not supporting inttypes.h or
